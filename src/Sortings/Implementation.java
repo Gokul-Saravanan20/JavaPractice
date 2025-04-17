2f3f51c4b869feffe10313120e3aef96a1784bfc
Gokul-Saravanan20 <gokulsaravanan758@gmail.com>
2025-04-17 23:10:42 +0530

    chore:selection sort completed

--- src/Sortings/Implementation.java
+++ src/Sortings/Implementation.java
@@ -1,14 +1,11 @@
 package Sortings;
 
-public class Implementaions implements Sorting{
-
+public class Implementation implements Sorting{
     @Override
     public void bubble(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             // It is called a pass
-
             boolean swapped = false; // checking the pass is swapped or not
-
             for (int j = 1; j < arr.length-i; j++) {
 
                 if(arr[j] < arr[j-1]){  //swap
@@ -16,20 +13,20 @@ public class Implementaions implements Sorting{
                     swapped = true;
                 }
             }
-
             if(!swapped) break;
         }
     }
 
     @Override
     public void selection(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            int max = getMax(arr,i, arr.length-i); // element I am going to replace
-            int index = arr.length-i; // the position I want to replace the element
-            
-
+        for (int i = 0; i < arr.length; i++){
+            int max = getMax(arr,arr.length-i); // element I am going to replace
+            int index = arr.length-i-1; // the position I want to replace the element
+            swap(arr,index,max);
         }
     }
+
+
 
 
     private void swap(int[] arr, int i, int j) {
@@ -38,13 +35,15 @@ public class Implementaions implements Sorting{
         arr[j] = temp;
     }
 
-    private int getMax(int[] arr, int start, int end) {
+
+    private int getMax(int[] arr, int end) {
         int max = 0;
-        for (int i = start; i < end; i++){
-            if(arr[i] > max){
+        for (int i = 0; i < end; i++){
+            if(arr[i] > arr[max]){
                 max = i;
             }
         }
         return max;
     }
 }
+
--- src/Sortings/MainForSorting.java
+++ src/Sortings/MainForSorting.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class MainForSorting {
     public static void main(String[] args) {
-        int[] arr = {6,5,4,3,2,1};
-        Sorting sorting = new Implementaions();
+        int[] arr = {6,1,2,5,4,3};
+        Sorting sorting = new Implementation();
 //        sorting.bubble(arr);
           sorting.selection(arr);
           //changed
